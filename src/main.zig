5b781ffff0fc20b825727bf8dcf0034ff8173161
Brian Frazho <befrazho@gmail.com>
2022-09-23 22:38:53 -0400

    fixed silly mistake from creating a buffer and returning it in the same method

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,14 @@
 const std = @import("std");
 const testing = std.testing;
 
-fn fizz_buzz(input: u64) ![]const u8 {
-    if(is_divisible_by_3(input) and is_divisible_by_5(input)){
+fn fizz_buzz(input: u64, buffer: []u8) ![]const u8 {
+    if (is_divisible_by_3(input) and is_divisible_by_5(input)) {
         return "FizzBuzz";
-    } else if(is_divisible_by_3(input)){
+    } else if (is_divisible_by_3(input)) {
         return "Fizz";
-    } else if(is_divisible_by_5(input)) {
+    } else if (is_divisible_by_5(input)) {
         return "Buzz";
     } else {
-        var buffer: [20]u8 = undefined;
         return try std.fmt.bufPrint(buffer[0..], "{d}", .{input});
     }
 }
@@ -23,46 +22,55 @@ fn is_divisible_by_5(input: u64) bool {
 }
 
 test "given 3 return Fizz" {
-    const result = try fizz_buzz(3);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(3, &buffer);
     try testing.expectEqualStrings(result, "Fizz");
 }
 
 test "given 1 return 1" {
-    const result = try fizz_buzz(1);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(1, &buffer);
     try testing.expectEqualStrings(result, "1");
 }
 
 test "given 2 return 2" {
-    const result = try fizz_buzz(2);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(2, &buffer);
     try testing.expectEqualStrings(result, "2");
 }
 
 test "given 6 return Fizz" {
-    const result = try fizz_buzz(6);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(6, &buffer);
     try testing.expectEqualStrings(result, "Fizz");
 }
 
 test "given 5 return Buzz" {
-    const result = try fizz_buzz(5);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(5, &buffer);
     try testing.expectEqualStrings(result, "Buzz");
 }
 
 test "given 10 return Buzz" {
-    const result = try fizz_buzz(10);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(10, &buffer);
     try testing.expectEqualStrings(result, "Buzz");
 }
 
 test "given 1000001 return 100001" {
-    const result = try fizz_buzz(1000001);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(1000001, &buffer);
     try testing.expectEqualStrings(result, "1000001");
 }
 
 test "given 15 return FizzBuzz" {
-    const result = try fizz_buzz(15);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(15, &buffer);
     try testing.expectEqualStrings(result, "FizzBuzz");
 }
 
 test "given 30 return FizzBuzz" {
-    const result = try fizz_buzz(30);
+    var buffer: [20]u8 = undefined;
+    const result = try fizz_buzz(30, &buffer);
     try testing.expectEqualStrings(result, "FizzBuzz");
 }
