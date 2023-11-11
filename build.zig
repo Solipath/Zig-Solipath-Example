382800a6b8841e8843b3e26736ad338f7b6bb3be
Brian <befrazho@gmail.com>
2023-11-11 15:19:31 -0500

    updated zig to 0.11.0 and added zig language server

--- build.zig
+++ build.zig
@@ -3,15 +3,23 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const lib = b.addStaticLibrary(.{
+        .name = "solipath-zig-example", 
+        .root_source_file = .{.path = "src/main.zig"},
+        .optimize = optimize,
+        .target = target
+    });
+    b.installArtifact(lib);
 
-    const lib = b.addStaticLibrary("solipath-zig-example", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
-
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file=.{.path = "src/main.zig"},
+        .optimize=optimize,
+        .target = target
+    });
+    const run_tests_artifact = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_tests_artifact.step);
 }
